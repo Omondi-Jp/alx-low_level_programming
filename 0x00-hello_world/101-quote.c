0db86e8f5973c9e347be7d136e110ec6dd0f019e
Juma Paul <jp0829673@gmail.com>
2022-09-08 16:00:19 -0700

    modified

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- *main - Entry poit 
+ *main- Entry poit 
  *
  *Return: Always 1 (Success)
  */
-int main (void)
+int main(void)
 {
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
