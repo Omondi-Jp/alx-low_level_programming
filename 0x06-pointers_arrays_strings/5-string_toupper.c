27b0939ca1e18d4aacb57bbd60e95509a9fe960a
Juma Paul <jp0829673@gmail.com>
2022-09-22 18:57:23 -0700

    m

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,11 +3,11 @@
 /**
  * string_toupper - Changes all lowercase letters
  *                  of a string to uppercase.
- * @str: The string to be changed.
+ * @a: The string to be changed.
  *
  * Return: A pointer to the changed string.
  */
-char *string_toupper(char *str)
+char *string_toupper(char *a)
 {
 	char *ptr = a;
 
