a654e84be5f135b3392519b0e1b910e6242cc50d
Juma Paul <jp0829673@gmail.com>
2022-10-24 16:11:38 -0700

    m

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,28 +1,55 @@
-#include "lists.h"
 #include <stdlib.h>
+#include <string.h>
+#include "lists.h"
 
 /**
- * free_list - free a list_t list
+ * add_node_end - adds a node to the end of a list_t list
  *
- * @head: head of list to free
+ * @head: node to add a node after
+ * @str: string to put in new node
  *
- * Return: void
+ * Return: address of new element
  */
-void free_list(list_t *head)
+list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *next;
+	char *newstr, *ptr;
+	list_t *newnode, *lastnode = NULL;
+	int len = 0;
 
-	if (head == NULL)
-		return;
-	*next = head->next;
-
-	while (head != NULL)
+	if (str != NULL)
 	{
-		if (head->str != NULL)
-			free(head->str);
-		free(head);
-		head = next;
-		if (head != NULL)
-			next = head->next;
+		ptr = (char *) str;
+		while (*ptr++)
+			len++;
+		newstr = malloc(sizeof(char) * (len + 1));
+		if (newstr == NULL)
+			return (NULL);
+
+		ptr = newstr;
+		while (*str)
+			*ptr++ = *str++;
 	}
+	else
+		newstr = NULL;
+
+	if (*head != NULL)
+	{
+		lastnode = *head;
+		while (lastnode->next != NULL)
+			lastnode = lastnode->next;
+	}
+	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
+	{
+		free(newstr);
+		return (NULL);
+	}
+
+	if (*head == NULL)
+		*head = newnode;
+	if (lastnode != NULL)
+		lastnode->next = newnode;
+	newnode->str = newstr;
+	newnode->len = len;
+	return (newnode);
 }
