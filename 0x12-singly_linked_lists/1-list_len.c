0cf02e4ac6dad1ab2af03ffb0ce54422a7b6ae0a
Juma Paul <jp0829673@gmail.com>
2022-10-24 14:55:19 -0700

    m

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,14 +7,9 @@
 /**
  * list_len - returns number of nodes starting at element h
  *
- *
- *
  * @h: first node of linked list to count from
  *
- *
- *
  * Return: number of nodes printed
- *
  */
 
 size_t list_len(const list_t *h)
